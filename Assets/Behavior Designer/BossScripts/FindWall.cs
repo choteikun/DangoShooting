0193d659c21e575abd34888cb02a2232af9696de
Choteikun <blackzxz77@gmail.com>
2022-04-18 08:41:06 +0800

    0

---
+++ Assets/Behavior Designer/BossScripts/FindWall.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+using BehaviorDesigner.Runtime;
+
+public class FindWall : Action
+{
+    
+}
